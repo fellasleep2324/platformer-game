0d16f1867f4fa93a3acf3f8b1f7e321d988932a8
fellasleep2324 <110793032+fellasleep2324@users.noreply.github.com>
2023-06-04 01:48:11 -0700

    idk_restart_cont.

--- 2D platformer game/Assets/Scripts/CanvasOn_Off.cs
+++ 2D platformer game/Assets/Scripts/CanvasOn_Off.cs
@@ -1,35 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class Control : MonoBehaviour
+public class CanvasOn_Off : MonoBehaviour
 {
+
     public GameObject Canvas;
+    // Start is called before the first frame update
     void Start()
     {
         
     }
-
- 
-    void Update()
-    {
-        
-    }
-    
     public void whenButtonClicked()
     {
         if (Canvas.activeInHierarchy == true)
             Canvas.SetActive(false);
         else
             Canvas.SetActive(true);
-    }    
-    public void ResetTheGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-    public void MainMenu()
+    // Update is called once per frame
+    void Update()
     {
-        SceneManager.LoadScene(0);
+        
     }
 }
