6afb45cac549e6bc43c14518046394b1a6e5ee85
Nirgy <sandhubalraj45@gmail.com>
2023-06-04 23:03:39 -0700

    Scene1CompleteBasically

--- 2D platformer game/Assets/Scripts/LightBandit1Move.cs
+++ 2D platformer game/Assets/Scripts/LightBandit1Move.cs
@@ -11,18 +11,18 @@ public class LightBandit1Move : MonoBehaviour
     public Transform Target;
     public bool isChasing; 
     public float chaseDistance;
-    public LightBanditHealthBar healthManager;
+    public LightBanditHealthBar health;
     
 
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(currentHealth = 0)
+        if(health.currentHealth == 0)
         {
             return;
         }
+    
         if(Vector2.Distance(Target.position,transform.position) <3f)
         {
             animator.SetTrigger("Attack");
